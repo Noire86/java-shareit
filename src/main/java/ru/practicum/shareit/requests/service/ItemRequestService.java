4de516ba235373b9bd2fc4514a11d54e3a6b91ff
Noire86 <gleb.iwanow2010@yandex.ru>
2022-10-10 03:16:14 +0500

    Cleanup

--- src/main/java/ru/practicum/shareit/requests/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/requests/service/ItemRequestService.java
@@ -8,9 +8,11 @@ import ru.practicum.shareit.requests.dto.ItemRequestExtendedDto;
 import java.util.List;
 
 public interface ItemRequestService {
-
     ItemRequestDto createRequest(Integer userId, ItemRequestCreationDto itemRequestCreationDto);
+
     List<ItemRequestExtendedDto> getPersonalItemRequests(Integer userId);
+
     List<ItemRequestExtendedDto> getAllItemRequestsPaged(Integer userId, Integer from, Integer size);
+
     ItemRequestExtendedDto getItemRequestById(Integer userId, Integer requestId);
 }
